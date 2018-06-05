a9b69b45e52c1d67343d2e9982bc1a33bfd8ec51
apoc64 <sschwedt@gmail.com>
2018-06-05 09:40:37 -0600

    tests admin can create genre

--- spec/features/admin_sees_genre_index_spec.rb
+++ spec/features/admin_sees_genre_index_spec.rb
@@ -18,4 +18,39 @@ describe 'admin visits genre index' do
     expect(page).to have_content(genre1.name)
     expect(page).to have_content(genre2.name)
   end
+
+  # As an admin user,
+  # When I visit the genre index page,
+  # I see a form to create a new genre.
+  it 'shows a form to create new genre' do
+    admin = User.create(username: "Dee", password: "password", role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit genres_path
+    expect(page).to have_button("Create Genre")
+  end
+  # As an admin user,
+  # When I visit the genre index page,
+  # And I fill out the form with a genre's name and click 'Create Genre',
+  # I return to the genre index page,
+  # I see all genre names from the database including the new genre I have just created.
+  it 'can create a new genre' do
+    genre1 = Genre.create(name: 'Action')
+    genre2 = Genre.create(name: 'Comedy')
+    admin = User.create(username: "Dee", password: "password", role: 1)
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit genres_path
+
+    name = 'Sci-Fi'
+    fill_in 'genre[name]', with: name
+    click_on 'Create Genre'
+
+    expect(current_path).to eq(genres_path)
+    expect(page).to have_content(name)
+    expect(page).to have_content(genre1.name)
+    expect(page).to have_content(genre2.name)
+  end
 end
