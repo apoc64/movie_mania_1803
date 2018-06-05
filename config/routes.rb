f4e162bc9503306782f56fbb60d556ebf3c16899
apoc64 <sschwedt@gmail.com>
2018-06-05 09:46:33 -0600

    admin can create genre

--- config/routes.rb
+++ config/routes.rb
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
 
   resources :actors, only: [:new, :create, :index]
 
-  resources :genres, only: [:index]
+  resources :genres, only: [:index, :create]
 
   namespace :admin do
     resources :categories, only: [:index]
