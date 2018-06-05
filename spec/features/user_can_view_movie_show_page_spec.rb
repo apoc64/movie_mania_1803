237ea3bc059c3f845a3f9219b7f8df1145ac8fc4
apoc64 <sschwedt@gmail.com>
2018-06-05 10:15:22 -0600

    movie show shows all genres for movie

--- spec/features/user_can_view_movie_show_page_spec.rb
+++ spec/features/user_can_view_movie_show_page_spec.rb
@@ -12,4 +12,24 @@ describe "user sees one movie" do
     expect(page).to have_content(movie.title)
     expect(page).to have_content(movie.description)
   end
+
+#   As a Visitor,
+#   When I visit a movie show page,
+#     I see genre names under a heading that says "Genres for this Movie:"
+#       and the genres listed are only the genres associated with this movie.
+#
+# (for testing purposes, I should see at least 2 genres listed for a movie)
+  it 'shows genres for the movie' do
+    director = Director.create(name: 'George Lucas')
+    movie = director.movies.create(title:'Star Wars Episode 4', description: 'A New Hope')
+    genre1 = Genre.create(name: 'Sci-Fi')
+    genre2 = Genre.create(name: 'Action')
+    movie.genres << genre1
+    movie.genres << genre2
+
+    visit movie_path(movie.slug)
+
+    expect(page).to have_content(genre1.name)
+    expect(page).to have_content(genre2.name)
+  end
 end
