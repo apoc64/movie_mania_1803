0a004546f1d00cd099d93c26ee105ade94f2026a
apoc64 <sschwedt@gmail.com>
2018-06-06 09:41:43 -0600

    cleans up comments

--- app/controllers/genres_controller.rb
+++ app/controllers/genres_controller.rb
@@ -4,11 +4,6 @@ class GenresController < ApplicationController
     @genre = Genre.new
   end
 
-  # def create
-  #   Genre.create(genre_params)
-  #   redirect_to genres_path
-  # end
-
   def show
     genre = Genre.find(params[:id])
     @movies = genre.movies
@@ -18,10 +13,4 @@ class GenresController < ApplicationController
     @lowest_rating = genre.lowest_rating
     @lowest_rated_movie = genre.movie_with_rating(@lowest_rating)
   end
-
-  # private
-  #
-  # def genre_params
-  #   params.require(:genre).permit(:name)
-  # end
 end
